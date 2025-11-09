067ba70a7c0fb4b6fcec8c2c4bedb97e434be3c3
sevketozurfali@gmail.com <sevketozurfali@gmail.com>
2025-11-09 13:34:09 +0300

    Finished weekly mission.

--- week44-year2025/weekly/todo/main.c
+++ week44-year2025/weekly/todo/main.c
@@ -2,53 +2,109 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+#include "mission.h"
 
+void clear_buffer (void)
+{
+    while ( getchar() != '\n' );
+}
 
+void sgets(char *ch) {
 
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+        *ch++ = (char)c;
+    }
 
+    *ch = '\0';
+}
 
+void print_missions(HMission hm) {
+    print_mission_list(hm);
+}
 
+void append(HMission hmission) {
+    printf("Mission :");
+    mission mis;
+    sgets(mis.title);
 
+    srand(time(NULL));
+
+    mis.id = rand() % 9000;
+    mis.completed = not_completed;
+
+    push_back(hmission, &mis);
+}
+
+void mark_mission(HMission hm) {
+    printf("Please enter id of the task would like to mark as completed :");
+    mission m;
+    char cid[16];
+    sgets(cid);
+    m.id = atoi(cid);
+
+    if (!mark_mission_as_completed(hm, &m)) {
+        printf("Error Couldn't mark as completed...\n");
+    }
+    else {
+        printf("Done\n");
+    }
+
+}
+
+void delete_mission(HMission hm) {
+
+    printf("Please enter the id of would like to delete:");
+    mission m;
+    char cid[16];
+    sgets(cid);
+    m.id = atoi(cid);
+
+    delete(hm, &m);
+}
 
 int main() {
 
-    int option = 0;
+    HMission hm = create_mission_list();
 
     while (true){
-        printf("\
-            1 - Add new mission \
-            2 - List all missions \
-            3 - Mark the mission as completed \
-            4 - Delete mission \
-            5 - Exit \
-        ");
 
+        printf(
+            "1 - Add new mission\n"
+            "2 - List all missions\n"
+            "3 - Mark the mission as completed\n"
+            "4 - Delete mission\n5 - Exit\n"
+            );
+
+        int option = 0;
+        printf("Please enter an option :");
         scanf("%d", &option);
+        clear_buffer();
+
 
         switch (option)
         {
         case 1:
-            append(hmission, mission);
+            append(hm);
             break;
         case 2:
-            list_all_missions(hmission);
+            print_missions(hm);
             break;
         case 3:
-            mark_mission(hmission, mission);
+            mark_mission(hm);
             break;
         case 4:
-            delete_mission(hmission, mission);
+            delete_mission(hm);
             break;
         case 5:
+                printf("Exiting...");
+                destroy_mission_list(hm);
             exit(EXIT_SUCCESS);
-        
         default:
             break;
         }
     }
-
-
-
 
     return 0;
 }
--- week44-year2025/weekly/todo/mission.h
+++ week44-year2025/weekly/todo/mission.h
@@ -2,6 +2,7 @@
 #define TODO_MISSION_H
 
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -9,12 +10,13 @@
 
 
 #define DEF_SIZE 20
+#define TITLE_SIZE 512
 
 
 typedef struct mission
 {
     int id;
-    char title[50];
+    char title[TITLE_SIZE];
     int completed;
 }mission;
 
@@ -25,12 +27,27 @@ typedef struct tagMission
     size_t size;
 }Mission, *HMission;
 
+enum isCompleted {
+    completed = 0,
+    not_completed = 1,
+};
 
-HMission create_mission_list();
+#define get_size(hmission)      ((hmission)->size)
 
-HMission create_mission_list_with_cap();
+HMission create_mission_list_with_default_cap(size_t cap);
+HMission create_mission_list(void);
+bool set_capacity(HMission);
 
+// void insert(HMission, const mission *mis, size_t index);
+void pop(HMission);
+HMission push_back(HMission, mission *mis);
+void delete(HMission, const mission *mis);
 
+void print_mission_list(HMission);
+void print_mission(const mission *mis);
+
+bool mark_mission_as_completed(HMission, const mission *mis);
+void destroy_mission_list(HMission);
 
 
 #endif
